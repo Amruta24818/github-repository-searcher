356f5763412b6cec87bab24c96972e0b68c0e33d
Amruta24818 <patilamrutaj@gmail.com>
2025-03-24 23:20:59 +0530

    Initial Commit

---
+++ src/main/java/com/example/github_repository_searcher/GithubRepositorySearcherApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_repository_searcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositorySearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubRepositorySearcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github_repository_searcher/GithubRepositorySearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_repository_searcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositorySearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
